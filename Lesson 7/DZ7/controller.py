6f30f54897f89642b8a171358bf2adb23d2a9805
Alex <89217796407@ya.ru>
2022-11-14 23:15:00 +0300

    Redact DZ7

--- Lesson 7/DZ7/controller.py
+++ Lesson 7/DZ7/controller.py
@@ -38,9 +38,9 @@ def choice_todo():
         data = export_data()
         item = search_data(word, data)
         if item != None:
-            print('Фамилия'.center(20),'Имя'.center(20),'Отчество'.center(20),'Дата рождения'.center(20),'Телефон'.center(20),'Категория'.center(20))
+            print('Фамилия'.center(20),'Имя'.center(20),'Отчество'.center(20),'Дата рождения'.center(20),'Телефон'.center(15),'Категория'.center(30))
             print('-'*130)
-            print(item[0].center(20),item[1].center(20),item[2].center(20),item[3].center(20),item[4].center(20),item[5].center(20))
+            print(item[0].center(20),item[1].center(20),item[2].center(20),item[3].center(20),item[4].center(15),item[5].center(30))
         else:
             print("Данные не обнаруженны")
 
